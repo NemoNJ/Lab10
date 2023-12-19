26218045dca3c0818af3346b69ee070613cf5ea1
NemoNJ <nemojirayu@gmail.com>
2023-12-19 16:06:23 +0700

    Finish

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,12 +6,14 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt"); 
+    cout << "-------------------- BOOM ---------------------"<<"\n";
+    string textline;
+    while(getline(source,textline)){
+       dest << textline << "\n";
+    }
+    cout << "-------------------- HA!! ---------------------";
     source.close();
     dest.close();
 	return 0;
